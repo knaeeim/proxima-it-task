ba98ff43101335ba48817bff23e440b47faa4578
Khairul Bashar Naeeim <mdkhairulb01@gmail.com>
2026-02-24 15:49:06 +0600

    Skeleton width adjusted with the new width of Inventory

--- src/app/(dashboard)/loading.tsx
+++ src/app/(dashboard)/loading.tsx
@@ -2,7 +2,7 @@ import { InventorySkeleton } from "@/components/inventorySkeleton";
 
 export default function Loading() {
     return (
-        <main className="p-6 lg:p-10 max-w-7xl mx-auto">
+        <main className="p-6 lg:p-10 w-[90%] mx-auto">
             <header className="mb-10">
                 <div className="h-10 w-48 bg-zinc-900 animate-pulse rounded-md mb-2" />
                 <div className="h-5 w-64 bg-zinc-900 animate-pulse rounded-md" />
