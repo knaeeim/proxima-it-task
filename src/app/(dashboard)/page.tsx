107804153aa8c2b7c314214de7c4ea64c15dd101
Khairul Bashar Naeeim <mdkhairulb01@gmail.com>
2026-02-24 15:54:08 +0600

    Minimal UI adjustment for better looking keep some spaces from the bottom

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -7,8 +7,8 @@ export default async function Home() {
     const allVehicles: Vehicle[] = data.products;
 
     return (
-        <main className="w-[90%] mx-auto">
-            <header className="my-10 flex flex-col justify-center items-center md:block">
+        <main className="w-[90%] mx-auto mt-10 my-20">
+            <header className="mb-10 flex flex-col justify-center items-center md:block">
                 <h1 className="text-3xl font-bold tracking-tight">Current Inventory</h1>
                 <p className="text-zinc-500">Explore our curated collection.</p>
             </header>
